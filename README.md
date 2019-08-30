027acaf5d734b972562e80d666cb17b53718db71
Ubuntu <ubuntu@ip-172-31-41-255.ec2.internal>
2019-08-30 15:30:36 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The World's Cookbook
+
+## An online repository of recipes created by people from all over the world
---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hello world"
+    
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP'),
+            port=int(os.getenv('PORT')),
+            debug=True)
